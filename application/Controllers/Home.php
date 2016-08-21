7d4a2359885359d9729dfcf0606594a8411d4419
SineMa <bock.christian@mincedmind.com>
2016-08-21 12:28:58 +0200

    select examples, even a nested one

--- application/Controllers/Home.php
+++ application/Controllers/Home.php
@@ -9,7 +9,41 @@ class Home extends \CodeIgniter\Controller
 			'version' => \CodeIgniter\CodeIgniter::CI_VERSION,
 		);
 
-		\Mongo\Client::instance()->setDb('sine');
+		// \Mongo\Client::instance()
+		// 	->collection('test')
+		// 	->insert(array('birds' => 'are blue'));
+
+		// \Mongo\Client::instance()
+		// 	->collection('test')
+		// 	->delete(array('birds' => 'are red'));
+
+		// \Mongo\Client::instance()
+		// 	->collection('test')
+		// 	->update(array('birds' => 'are black', 'upsert' => true), array('birds' => 'are green'));
+
+		// \Mongo\Client::instance()
+		// 	->collection('test')
+		// 	->update(array('birds' => 'are yellow'), array('birds' => 'are black'));
+
+		// $data = array(
+		// 	'birds' => array(
+		// 		'color' => 'black',
+		// 		'size' => array(0, 1, 2, 2, 4, 5)
+		// 	),
+		// 	'spiders' => 'are eeeeeeeek'
+		// );
+		// \Mongo\Client::instance()
+		// 	->collection('test')
+		// 	->insert($data);
+
+		// \Mongo\Client::instance()
+		// 	->collection('test')
+		// 	->select(array('birds' => 'are red'));
+
+
+		// \Mongo\Client::instance()
+		// 	->collection('test')
+		// 	->select(array('birds.color' => 'black'));
 
 		return \Twig::instance()->display('app.html', $data);
 	}
